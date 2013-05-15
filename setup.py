dd9f4bd6278a1d22e3fe9107568fe2181c343bf2
sammyrulez <sammyrulez@gmail.com>
2013-05-15 12:09:45 +0200

    fixed packages

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='sammyrulez',
     url='https://github.com/sammyrulez/django_pydev_command',
     license='BSD',
-	packages = ['pydev'],
+	packages = ['pydev','pydev.management','pydev.management.commands','pydev.management.commands.resources'],
 	package_dir = {'': 'src'},  
     install_requires=['Django>=1.4']
 )
